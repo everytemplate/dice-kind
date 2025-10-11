dbaf2fd753f240dad3f11b1fd1eb881e3009a29b
Brice <briceyan3.0@gmail.com>
2025-10-11 18:24:56 +0800

    chore: use kind-as-v0.1.4

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,9 +1,9 @@
 @kind
 class Dice {
-  face: Bytes32;
+  face: Info;
 
   meta(): Json {
-    let data = json.create("{}");
+    let data = json.create();
     json.setString(data, "/name", `Dice #${object.id()}`);
     json.setString(data, "/description", "A dice");
     json.setString(data, "/image", "facet://picture");
@@ -13,57 +13,57 @@ class Dice {
   picture(): Image {
     let canvas = image.create(800, 800, 2);
     // Draw white background with black border for the dice
-    const COLOR_WHITE = new PixelRgb8(255, 255, 255);
-    const COLOR_BLACK = new PixelRgb8(0, 0, 0);
-    const COLOR_RED = new PixelRgb8(255, 0, 0);
-    image.drawFilledRectMut(canvas, new Rect(0, 0, 800, 800), COLOR_WHITE);
-    image.drawHollowRectMut(canvas, new Rect(50, 50, 700, 700), COLOR_BLACK);
+    const WHITE = Pixel.Rgb8(255, 255, 255);
+    const BLACK = Pixel.Rgb8(0, 0, 0);
+    const RED = Pixel.Rgb8(255, 0, 0);
+    image.drawFilledRectMut(canvas, new Rect(0, 0, 800, 800), WHITE);
+    image.drawHollowRectMut(canvas, new Rect(50, 50, 700, 700), BLACK);
 
-    let dots = (this.face.extractU8(31) % 6) + 1;
+    let dots = (this.face.u8(31) % 6) + 1;
     switch (dots) {
       case 1:
         // Center dot
-        image.drawFilledCircleMut(canvas, new PointI32(400, 400), 50, COLOR_RED);
+        image.drawFilledCircleMut(canvas, Point.I32(400, 400), 50, RED);
         break;
 
       case 2:
         // Top left and bottom right
-        image.drawFilledCircleMut(canvas, new PointI32(200, 200), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(600, 600), 50, COLOR_BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(200, 200), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(600, 600), 50, BLACK);
         break;
 
       case 3:
         // Diagonal dots plus center
-        image.drawFilledCircleMut(canvas, new PointI32(200, 200), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(400, 400), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(600, 600), 50, COLOR_BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(200, 200), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(400, 400), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(600, 600), 50, BLACK);
         break;
 
       case 4:
         // Four corners
-        image.drawFilledCircleMut(canvas, new PointI32(200, 200), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(600, 200), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(200, 600), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(600, 600), 50, COLOR_BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(200, 200), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(600, 200), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(200, 600), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(600, 600), 50, BLACK);
         break;
 
       case 5:
         // Four corners plus center
-        image.drawFilledCircleMut(canvas, new PointI32(200, 200), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(600, 200), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(400, 400), 50, COLOR_RED);
-        image.drawFilledCircleMut(canvas, new PointI32(200, 600), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(600, 600), 50, COLOR_BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(200, 200), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(600, 200), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(400, 400), 50, RED);
+        image.drawFilledCircleMut(canvas, Point.I32(200, 600), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(600, 600), 50, BLACK);
         break;
 
       case 6:
         // Six dots in 2x3 pattern
-        image.drawFilledCircleMut(canvas, new PointI32(200, 200), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(200, 400), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(200, 600), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(600, 200), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(600, 400), 50, COLOR_BLACK);
-        image.drawFilledCircleMut(canvas, new PointI32(600, 600), 50, COLOR_BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(200, 200), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(200, 400), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(200, 600), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(600, 200), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(600, 400), 50, BLACK);
+        image.drawFilledCircleMut(canvas, Point.I32(600, 600), 50, BLACK);
         break;
     }
     return canvas;
